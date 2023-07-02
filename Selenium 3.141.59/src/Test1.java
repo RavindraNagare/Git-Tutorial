819bf38dc1709077a2d3db20184e126dfc4708f2
ravinagare <110158583+ravinagare@users.noreply.github.com>
2023-07-02 12:11:41 +0530

    added aditional check

--- Selenium 3.141.59/src/Test1.java
+++ Selenium 3.141.59/src/Test1.java
@@ -2,8 +2,15 @@
 public class Test1 {
 
 	public static void main(String[] args) {
+		String s="yes";
+				if(s=="yes" || s== "Yes")
+				{
 		System.out.println("Ravindra is my name");
-
-	}
+				}
+				else
+				{
+					System.out.println("Ravindra is not may name");
+				}
+}
 
 }
